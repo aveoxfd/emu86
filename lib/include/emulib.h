4013b8fce31b334d8910542d544842327fa5a2de
aveoxfd <krekerbruh@hotmail.com>
2025-10-09 14:11:37 +0300

    develop update

--- lib/include/emulib.h
+++ lib/include/emulib.h
@@ -2,72 +2,7 @@
 #define EMULIB_H
 
 #include "emutool.h"
-
-#define PREFETCH_QUEUE_SIZE_MACRO 6
-#define PREFIX_CS_MACRO 0x2E
-#define PREFIX_DS_MACRO 0x3E
-#define PREFIX_ES_MACRO 0x26
-#define PREFIX_SS_MACRO 0x36
-
-typedef enum{
-    MICRO_PLUG,
-    MICRO_LOAD_REG_IMM16,
-    MICRO_FETCH_MODRM,
-    MICRO_LOAD_REG_RM,
-    MICRO_LOAD_RM_REG,
-    MICRO_LOAD_SEGREG_RM,
-    MICRO_LOAD_RM_SEGREG,
-    MICRO_LOAD_REG_IMM8,
-    MICRO_LOAD_REG8_RM8,
-    MICRO_LOAD_RM8_REG8,
-    MICRO_LOAD_MEM_REG,
-    MICRO_LOAD_MEM_AX,
-    MICRO_ADD_AL_IMM8,
-    MICRO_LOAD_AL_MEM,
-    MICRO_HLT,
-    MICRO_END,
-    MICRO_UNSUPPORTED
-}Uop_instruct;
-
-typedef enum{
-    CS,
-    DS,
-    ES,
-    SS,
-    SR_COUNT
-}Segment_registers;
-typedef enum{
-    AX, 
-    CX, 
-    DX, 
-    BX, 
-    SP, 
-    BP, 
-    SI, 
-    DI,
-    GPR_COUNT
-}General_purpose_registers;
-typedef enum{
-    IP,
-    FLAGS,
-    S_COUNT,
-}System_registers;
-
-typedef struct CPU{
-    uint_16 general_purpose_registers[GPR_COUNT];   //ax, cx, dx, bx, sp, bp, si, di
-    uint_16 segments_registers[SR_COUNT];           //cs, ds, es, ss
-    uint_16 system_registers[S_COUNT];              //ip, flags
-
-    uint_8  prefetch_queue[PREFETCH_QUEUE_SIZE_MACRO];  // FF FF FF FF FF FF FF FF
-    uint_16 prefetch_head;                              // 
-    uint_16 prefetch_count;                             //
-
-    uint_8  opcode;                                 //opcode shadow register
-    uint_8  modrm;                                  //modr/m shadow register
-    uint_8  segment;                                //shadow select segment register
-    uint_16 disp;                                   //displacement
-    uint_16 base;                                   //base
-    uint_8  reset;                                  //on start value
-}CPU;
+#include "types.h"
+#include "../../src/cpu/cpu"
 
 #endif
---
+++ src/emu.c
@@ -0,0 +1,23 @@
+#include "../lib/include/emulib.h"
+
+extern uint_8 RAM[1024*1024];
+
+int main(){
+    uint_8 code[] = {
+        0x04, 0x01,                 //add al, 0x01
+        0xB4, 0xFF,                 //mov ah, 0xFF
+        0x04, 0x02,                 //add al, 0x02
+        0xB0, 0xFF,                 //mov al, 0xFF
+        0xB8, 0x00, 0xFF,           //mov ax, 0xFF00
+        0xA3, 0x00, 0x00,           //mov [0x0000], ax
+        0x8B, 0x0E, 0x00, 0x00,     //mov cx, [0x0000]      
+        0x90,                       //nop
+        0xF4                        //hlt               
+    };
+    memorycopy(RAM, code, sizeof(code));
+    raminfo(RAM, 512);
+    CPU cpu;
+    init_cpu(&cpu);
+    run(&cpu);
+    return 0;
+}
